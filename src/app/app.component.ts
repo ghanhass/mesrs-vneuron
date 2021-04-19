db49815e05f0b27cc0b0b26e2aabd4bc2e01cd40
hgh <hassen.ghanmi@vneuron.tn>
2021-04-19 02:23:52 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,9 +35,9 @@ ChartOptions1: any =  {
         radius: '50%',
         center: ['50%', '50%'],
         data: [
-          { value: 15, name: 'En attente de versement', id: 1},
-          { value: 25, name: 'En cours', id: 2},
-          { value: 60, name: 'Clôturé', id: 3 },
+          { value: 15, name: 'En attente de versement'},
+          { value: 25, name: 'En cours'},
+          { value: 60, name: 'Clôturé'},
         ],
         itemStyle: {
           emphasis: {
@@ -85,7 +85,10 @@ ChartOptions2: any = {
     },
     xAxis: {
         type: 'value',
-        max: 100
+        max: 100,
+        axisLabel: {
+          formatter: '{value} %'
+        },
     },
     yAxis: {
         type: 'category',
@@ -95,6 +98,11 @@ ChartOptions2: any = {
         {
           name: 'Avancement des projets',
           type: 'bar',
+          
+          showBackground: true,
+          backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)'
+          },
           
           label: {
               show: true,
@@ -108,12 +116,137 @@ ChartOptions2: any = {
     ]
 };
 
+ChartOptions3: any =  {
+        
+  color: ["#ff0000", "#ffc000", "#538135"],
+  tooltip: {
+    trigger: 'item',
+    formatter: '{b} : {d}%',
+    //formatter: '{a} <br/>{b} : {c} ({d}%)',
+    right:"right"
+  },
+  legend: {
+    orient: 'horizontal',
+    center: 'center',
+    bottom: "bottom",
+    data: ['Des projets en retard', 'Des projets ont eu des deviations en terme de budget', 'Des projets non pas encore reçu les virements des tranches'],
+    textStyle: {
+      color: "#000000",
+    },
+    selectedMode:false
+  },
+  series: [
+    {
+      //name: 'Countries',
+      type: 'pie',
+      radius: '50%',
+      center: ['50%', '50%'],
+      data: [
+        { value: 26, name: 'Des projets en retard'},
+        { value: 37, name: 'Des projets ont eu des deviations en terme de budget'},
+        { value: 37, name: 'Des projets non pas encore reçu les virements des tranches'},
+      ],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: "#8c8c8c",
+        },
+      },
+      label: {
+        normal: {
+          textStyle: {
+            color: "#000000",
+          },
+        },
+      },
+      labelLine: {
+        normal: {
+          lineStyle: {
+            color: "#8c8c8c",
+          },
+        },
+      },
+    },
+  ],
+};
+
+ChartOptions4: any = {
+  color:["#5470c6"],
+  tooltip: {
+      trigger: 'item',
+      /*axisPointer: {            // Use axis to trigger tooltip
+          type: 'shadow'        // 'shadow' as default; can also be 'line' or 'shadow'
+      }*/
+      formatter: '{b}: {c}%',
+  },
+  legend: {
+      selectedMode:false,
+      show:false
+  },
+  grid: {
+      /*left: '3%',
+      right: '4%',
+      bottom: '3%',*/
+      left:"3%",
+      right:"3%",
+      top:"7%",
+      containLabel: true
+  },
+  xAxis: {
+      type: 'category',
+      data: [
+        "Appui à une meilleure connexion au marché de l'emploi",
+        "Université innovante et entrepreneuriale",
+        "Services aux étudiants",
+        "Gouvernance et Capacité de Gestion",
+        "Assurance qualité & accréditation"
+      ],
+      axisLabel: {
+        overflow: "break",
+        height: 300,
+        width:90,
+        interval:0,
+        align:"center",
+        //verticalAlign:"bottom",
+        //rotate:-75
+      },
+  },
+  yAxis: {
+      type: 'value',
+      max: 100,
+      axisLabel: {
+        formatter: '{value} %'
+      },
+  },
+  series: [
+      {
+        name: 'Avancement des projets',
+        type: 'bar',
+        
+        label: {
+            show: true,
+            formatter: '{c}%',
+        },
+        emphasis: {
+            focus: 'series'
+        },
+        data: [60, 20, 10, 50, 50]
+      },
+  ]
+};
+
 selectedDataIndex1:number = undefined;
+
+selectedDataIndex3:number = undefined;
+
 echart2Title: string = "";
 
-@ViewChild("etatAvancementParStatutDetail") etatAvancementParStatutDetail: ElementRef;
+@ViewChild("etatProjetParStatutDetail") etatProjetParStatutDetail: ElementRef;
+@ViewChild("etatProjetParStatutDetailChart")etatProjetParStatutDetailChart: LineChartComponent;
 
-@ViewChild("etatAvancementParStatutDetailChart")etatAvancementParStatutDetailChart: LineChartComponent;
+@ViewChild("etatProjetParCauseAlerteDetail1") etatProjetParCauseAlerteDetail1: ElementRef;
+@ViewChild("etatProjetParCauseAlerteDetailChart1")etatProjetParCauseAlerteDetailChart1: LineChartComponent;
 
 constructor(private mockServerService: MockServerService) {}
 
@@ -121,24 +254,22 @@ ngOnInit(){
   
 }
 
-onEChartRender(data){
-}
 
 onEChart1DataSelect(data){
   console.log("onEChart1DataSelect data = ", data);
   this.selectedDataIndex1 = data;
-  let etatAvancementParStatutDetailEl: HTMLElement = this.etatAvancementParStatutDetail.nativeElement;
-  if(etatAvancementParStatutDetailEl){
-    etatAvancementParStatutDetailEl.style.opacity = "1";
+  let etatProjetParStatutDetailEl: HTMLElement = this.etatProjetParStatutDetail.nativeElement;
+  if(etatProjetParStatutDetailEl){
+    etatProjetParStatutDetailEl.classList.remove("hidden-diagram");
 
     let echart1Data = this.ChartOptions1.series[0].data[this.selectedDataIndex1];
 
     let echart1DataName = echart1Data.name;
-    this.echart2Title = "Projets en état '"+echart1DataName+"'";
+    this.echart2Title = "Projets en statut '"+echart1DataName+"'";
 
-    etatAvancementParStatutDetailEl.querySelector(".title").textContent = this.echart2Title;
+    etatProjetParStatutDetailEl.querySelector(".title").textContent = this.echart2Title;
   
-    let newEchart2Data = (this.mockServerService.db[1].data as Array<any>).filter((el)=>{
+    let newEchart2Data = (this.mockServerService.db[0].data as Array<any>).filter((el)=>{
       return echart1DataName == el.dependencyDiagramDataName;
     });
 
@@ -152,7 +283,7 @@ onEChart1DataSelect(data){
     this.ChartOptions2.yAxis.data = projectNames;
     this.ChartOptions2.series[0].data = dataValues;
 
-    this.etatAvancementParStatutDetailChart.chartInstance.setOption(this.ChartOptions2);
+    this.etatProjetParStatutDetailChart.chartInstance.setOption(this.ChartOptions2);
 
 
     console.log("echart1DataName = ", echart1DataName);
@@ -164,9 +295,63 @@ onEChart1DataSelect(data){
 onEChart1DataUnselect(){
   this.selectedDataIndex1 = undefined;
 
-  let etatAvancementParStatutDetailEl: HTMLElement = this.etatAvancementParStatutDetail.nativeElement;
-  if(etatAvancementParStatutDetailEl){
-    etatAvancementParStatutDetailEl.style.opacity = "0";
+  let etatProjetParStatutDetailEl: HTMLElement = this.etatProjetParStatutDetail.nativeElement;
+  if(etatProjetParStatutDetailEl){
+    etatProjetParStatutDetailEl.classList.add("hidden-diagram");
+  }
+}
+
+onEChart3DataSelect(data){
+  console.log("onEChart3DataSelect data = ", data);
+  this.selectedDataIndex3 = data;
+  let etatProjetParCauseAlerteDetail1_EL: HTMLElement = this.etatProjetParCauseAlerteDetail1.nativeElement;
+  if(etatProjetParCauseAlerteDetail1_EL){
+    etatProjetParCauseAlerteDetail1_EL.classList.remove("hidden-diagram");
+
+    let echart3Data = this.ChartOptions3.series[0].data[this.selectedDataIndex3];
+
+    let echart3DataName = echart3Data.name;
+    //this.echart2Title = "Projets en statut '"+echart1DataName+"'";
+
+    let title = {
+      subtext: echart3DataName,
+      //subtext: echart3DataName,
+      left: 'center',
+      textStyle: {
+        overflow: "break"
+      }
+    };
+    etatProjetParCauseAlerteDetail1_EL.querySelector(".title").textContent = "Pourcentages des bénéficiaires par catégorie";
+  
+    let newEchart4Data = (this.mockServerService.db[1].data as Array<any>).filter((el)=>{
+      return echart3DataName == el.dependencyDiagramDataName;
+    });
+
+    let projectNames = newEchart4Data.map((el)=>{return el.name});
+    let dataValues = newEchart4Data.map((el)=>{return el.data});
+
+    console.log("projectNames = ", projectNames);
+
+    console.log("dataValues = ", dataValues);
+
+    this.ChartOptions4.title = title;
+    this.ChartOptions4.xAxis.data = projectNames;
+    this.ChartOptions4.series[0].data = dataValues;
+
+    this.etatProjetParCauseAlerteDetailChart1.chartInstance.setOption(this.ChartOptions4);
+
+
+    console.log("echart3DataName = ", echart3DataName);
+    console.log("newEchart4Data = ", newEchart4Data);
+  }
+}
+
+onEChart3DataUnselect(){
+  this.selectedDataIndex3 = undefined;
+
+  let etatProjetParCauseAlerteDetail1_El: HTMLElement = this.etatProjetParCauseAlerteDetail1.nativeElement;
+  if(etatProjetParCauseAlerteDetail1_El){
+    etatProjetParCauseAlerteDetail1_El.classList.add("hidden-diagram");
   }
 }
 
--- src/app/mock-server.service.ts
+++ src/app/mock-server.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class MockServerService {
 
   db = [
-    {
-      diagramName: "etat-avancement-par-statut",
+    /*{
+      diagramName: "etat-projet-par-statut",
       data:[
         {
           name: "En attente de versement",
@@ -22,89 +22,186 @@ export class MockServerService {
           data: 60
         }
       ]
-    },
+    },*/
     {
-      diagramName: "etat-avancement-par-statut-detail",
+      diagramName: "etat-projet-par-statut-detail",
       data:[
         {
           name: "Projet 1",
           data: 30,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En attente de versement",
         },
         {
           name: "Projet 2",
           data: 40,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En attente de versement",
         },
         {
           name: "Projet 3",
           data: 20,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En attente de versement",
         },
         {
           name: "Projet 4",
           data: 50,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En attente de versement",
         },
         /****************/
         {
           name: "Projet 1",
           data: 45,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En cours",
         },
         {
           name: "Projet 2",
           data: 70,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En cours",
         },
         {
           name: "Projet 3",
           data: 80,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En cours",
         },
         {
           name: "Projet 4",
           data: 10,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En cours",
         },
         {
           name: "Projet 5",
           data: 70,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "En cours",
         },
         /****************/
         {
           name: "Projet 1",
           data: 100,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "Clôturé",
         },
         {
           name: "Projet 2",
           data: 100,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "Clôturé",
         },
         {
           name: "Projet 3",
           data: 100,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "Clôturé",
         },
         {
           name: "Projet 4",
           data: 100,
-          dependencyDiagramName: "etat-avancement-par-statut",
+          dependencyDiagramName: "etat-projet-par-statut",
           dependencyDiagramDataName: "Clôturé",
+        }
+      ]
+    },
+    {
+      diagramName: "etat-projet-par-cause-alerte-detail",
+      data:[
+        {
+          name: "Appui à une meilleure connexion au marché de l'emploi",
+          data: 60,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets en retard",
+        },
+        {
+          name: "Université innovante et entrepreneuriale",
+          data: 20,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets en retard",
+        },
+        {
+          name: "Services aux étudiants",
+          data: 10,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets en retard",
+        },
+        {
+          name: "Gouvernance et Capacité de Gestion",
+          data: 50,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets en retard",
+        },
+        {
+          name: "Assurance qualité & accréditation",
+          data: 50,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets en retard",
+        },
+        /*************/
+        {
+          name: "Appui à une meilleure connexion au marché de l'emploi",
+          data: 40,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets ont eu des deviations en terme de budget",
+        },
+        {
+          name: "Université innovante et entrepreneuriale",
+          data: 30,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets ont eu des deviations en terme de budget",
+        },
+        {
+          name: "Services aux étudiants",
+          data: 25,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets ont eu des deviations en terme de budget",
+        },
+        {
+          name: "Gouvernance et Capacité de Gestion",
+          data: 70,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets ont eu des deviations en terme de budget",
+        },
+        {
+          name: "Assurance qualité & accréditation",
+          data: 80,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets ont eu des deviations en terme de budget",
+        },
+        /*****************/
+        {
+          name: "Appui à une meilleure connexion au marché de l'emploi",
+          data: 85,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets non pas encore reçu les virements des tranches",
+        },
+        {
+          name: "Université innovante et entrepreneuriale",
+          data: 10,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets non pas encore reçu les virements des tranches",
+        },
+        {
+          name: "Services aux étudiants",
+          data: 35,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets non pas encore reçu les virements des tranches",
+        },
+        {
+          name: "Gouvernance et Capacité de Gestion",
+          data: 65,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets non pas encore reçu les virements des tranches",
+        },
+        {
+          name: "Assurance qualité & accréditation",
+          data: 65,
+          dependencyDiagramName: "etat-projet-par-cause-alerte",
+          dependencyDiagramDataName: "Des projets non pas encore reçu les virements des tranches",
         }
       ]
     }
